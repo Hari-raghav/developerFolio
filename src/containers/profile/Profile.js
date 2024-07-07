8bb9d055ee3f3a649e6aa9b1456bbc0951eb8050
Hari-raghav <harii.raghav@gmail.com>
2024-07-07 10:55:09 +0530

    Update Profile.js

--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -9,32 +9,32 @@ const GithubProfileCard = lazy(() =>
 );
 export default function Profile() {
   const [prof, setrepo] = useState([]);
-  // function setProfileFunction(array) {
-  //   setrepo(array);
-  // }
+  function setProfileFunction(array) {
+    setrepo(array);
+  }
 
   useEffect(() => {
-    // if (openSource.showGithubProfile === "true") {
-    //   const getProfileData = () => {
-    //     fetch("/profile.json")
-    //       .then(result => {
-    //         if (result.ok) {
-    //           return result.json();
-    //         }
-    //       })
-    //       .then(response => {
-    //         setProfileFunction(response.data.user);
-    //       })
-    //       .catch(function (error) {
-    //         console.error(
-    //           `${error} (because of this error GitHub contact section could not be displayed. Contact section has reverted to default)`
-    //         );
-    //         setProfileFunction("Error");
-    //         openSource.showGithubProfile = "false";
-    //       });
-    //   };
-    //   getProfileData();
-    // }
+    if (openSource.showGithubProfile === "true") {
+      const getProfileData = () => {
+        fetch("/profile.json")
+          .then(result => {
+            if (result.ok) {
+              return result.json();
+            }
+          })
+          .then(response => {
+            setProfileFunction(response.data.user);
+          })
+          .catch(function (error) {
+            // console.error(
+            //   `${error} (because of this error GitHub contact section could not be displayed. Contact section has reverted to default)`
+            // );
+            setProfileFunction("Error");
+            openSource.showGithubProfile = "false";
+          });
+      };
+      getProfileData();
+    }
   }, []);
   if (
     openSource.display &&
