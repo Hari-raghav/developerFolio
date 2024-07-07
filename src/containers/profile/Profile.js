f95219e3e059782e509dc1f595f7645b7fbd1190
Hari-raghav <harii.raghav@gmail.com>
2024-07-07 10:48:27 +0530

    Update Profile.js

--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -14,27 +14,27 @@ export default function Profile() {
   }
 
   useEffect(() => {
-    if (openSource.showGithubProfile === "true") {
-      const getProfileData = () => {
-        fetch("/profile.json")
-          .then(result => {
-            if (result.ok) {
-              return result.json();
-            }
-          })
-          .then(response => {
-            setProfileFunction(response.data.user);
-          })
-          .catch(function (error) {
-            console.error(
-              `${error} (because of this error GitHub contact section could not be displayed. Contact section has reverted to default)`
-            );
-            setProfileFunction("Error");
-            openSource.showGithubProfile = "false";
-          });
-      };
-      getProfileData();
-    }
+    // if (openSource.showGithubProfile === "true") {
+    //   const getProfileData = () => {
+    //     fetch("/profile.json")
+    //       .then(result => {
+    //         if (result.ok) {
+    //           return result.json();
+    //         }
+    //       })
+    //       .then(response => {
+    //         setProfileFunction(response.data.user);
+    //       })
+    //       .catch(function (error) {
+    //         console.error(
+    //           `${error} (because of this error GitHub contact section could not be displayed. Contact section has reverted to default)`
+    //         );
+    //         setProfileFunction("Error");
+    //         openSource.showGithubProfile = "false";
+    //       });
+    //   };
+    //   getProfileData();
+    // }
   }, []);
   if (
     openSource.display &&
